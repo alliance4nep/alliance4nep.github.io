9e2067a697acc7a078a80b8f265a422f9319f94e
Dijup Tuladhar <dezudas@gmail.com>
2021-05-20 20:19:51 +0545

    script for media

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,26 @@
-$(function () {
-    //define an object, you can both with or without quations for the keys.
-    var employee = { name: "John Johnson", "street": "Oslo West 16", phone: "555 1234567" };
+var dataJSON = [
+    {
+        "title": "Press Release - Covid Alliance for Nepal - UK",
+        "date": "May 18, 2021",
+        "desc": "Leading Public Figures Call on UK Government for Oxygen for Nepal",
+        "linklabel": "read more",
+        "link": "https://alliance4nep.github.io/press-release-20210518.html"
+    },
+    {
+        "title": "‘Urgent: Oxygen Needed’: Nepalis mobilise to take charge in Covid crisis",
+        "date": "May 18, 2021",
+        "desc": "Pete Pattison",
+        "linklabel": "read more",
+        "link": "https://www.theguardian.com/global-development/2021/may/18/urgent-oxygen-needed-nepalis-mobilise-to-take-charge-in-covid-crisis"
+    }
+];
 
-    //use JSON.stringify to convert it to json string
-    var jsonstring = JSON.stringify(employee);
-    var jsonobject = JSON.parse(jsonstring);
-    var info = '<ul><li>Name:' + jsonobject.name + '</li><li>Street:' + jsonobject.street + '</li><li>Phone:' + jsonobject.phone + '</li></ul>';
-    $("#result").append(info);
-});
+// var dataObject = JSON.parse(dataJSON)
+// console.log(dataObject);
+
+dataJSON.forEach(buildNewList);
+
+function buildNewList(item, index) {
+    document.getElementById("mediaList").appendChild('<div class="card"><div class="card-body"><h5 class="card-title">' + item.title + '</h5><h6 class="card-subtitle mb-2 text-muted"> ' + item.date + '</h6><p class="card-text">' + item.desc + ' </p><a href="' + item.link + '" class="card-link">' + item.linklabel + '</a></div></div>');
+    //document.getElementById("mediaList").appendChild (`<div class="card"><div class="card-body"><h5 class="card-title">${item.title}</h5><h6 class="card-subtitle mb-2 text-muted">${item.date}</h6><p class="card-text">${item.desc}</p><a href="${item.link}" class="card-link">${item.linklabel}</a></div></div>`);
+}
