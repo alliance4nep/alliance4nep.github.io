401b76130635b18cd6cd5aadb709b7a1df9b0034
Dijup Tuladhar <dezudas@gmail.com>
2021-05-22 17:23:08 +0545

    Site update with Banner

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+/*=====================================
+Nav Bar
+======================================= */
 const navJSON = [{
         "label": "Home",
         "link": "/",
@@ -27,19 +30,19 @@ const navJSON = [{
         "link": "/#help",
     }
 ];
-
-
 function renderNav(currentpage) {
 
     const buildNav = (item, index) => {
         const isActive = item.label === currentpage ? 'active' : '';
-        const navs = `<li class="nav-item ${isActive}"><a class="nav-link" href="${item.link}">${item.label}</a></li>`;
+        const navs = `<li class="nav-item ${isActive}"><a class="nav-link ${isActive}" href="${item.link}">${item.label}</a></li>`;
         document.getElementById("render_nav").insertAdjacentHTML('beforeend', navs);
     }
 
     navJSON.forEach(buildNav);
 }
-
+/*=====================================
+Remote content
+======================================= */
 function render(url, insertCallback) {
     fetch(url)
         .then(r => r.json())
@@ -47,3 +50,29 @@ function render(url, insertCallback) {
             data.rows.forEach(insertCallback);
         });
 }
+/*=====================================
+Sticky
+======================================= */
+window.onscroll = function () {
+    var header_navbar = document.getElementById("header_navbar");
+    var logo = document.querySelector("img#logo");
+    var sticky = header_navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+        logo.setAttribute("src", "images/logo.svg")
+    } else {
+        header_navbar.classList.remove("sticky");
+        logo.setAttribute("src", "images/logo-white.svg")
+    }
+
+    //
+    //
+    // // show or hide the back-top-top button
+    // var backToTo = document.querySelector(".back-to-top");
+    // if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //     backToTo.style.display = "block";
+    // } else {
+    //     backToTo.style.display = "none";
+    // }
+};
